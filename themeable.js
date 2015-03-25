2995564e0dc981e9e359569439b3eb96ee35e456
Sebastian Mellor <s.j.i.mellor@ncl.ac.uk>
2015-03-25 19:08:53 +0000

    fix a typo, one class

--- themeable.js
+++ themeable.js
@@ -20,45 +20,35 @@ $(function(){
             $('#Themable').html(
                 '<a>'+classes[tag].join('</a> <a>')+'</a>'
             );
-            $('#Themable').css({top: $that.offset().top-20, left: $that.offset().left, opacity: 1});
+            $('#Themeable').css({top: $that.offset().top-20, left: $that.offset().left, opacity: 1});
         }, 200);
     });
-    $('#Themable').on('mouseover', function(){
+    $('#Themeable').on('mouseover', function(){
         clearTimeout(moveTimer);
     });
     $('body').on('keyup', function(){
-        $('#Themable').css({opacity: 0});
+        $('#Themeable').css({opacity: 0});
     });
     
     // interact with toolbar
-    $('#Themable').on('click', 'a', function(){
+    $('#Themeable').on('click', 'a', function(){
         var tag = $that[0].tagName.toLowerCase();
         console.log(classes[tag].join(' '));
         activeElement.removeClass(classes[tag].join(' ')).addClass($(this).text());
     });
     
     // download function
-    $('body').append('<a id="ThemableDownload" style="display:none"></a>');
+    $('body').append('<a id="ThemeableDownload" style="display:none"></a>');
     
-    $('body').on('keypress', function(e){
-        // Ctrl+S
-        if ((event.which == 115 && event.ctrlKey) || (event.which == 19)) {
-            console.log(e.keyCode);
-            console.log('save');
-            e.preventDefault();
-            $('[download]')[0].click();
-            return false;
-        }
-    });
     $(window).bind('keydown', function(event) {
         if (event.ctrlKey || event.metaKey) {
             if (String.fromCharCode(event.which).toLowerCase() == 's') {
                 event.preventDefault();
                 var title = $($('h1')[0]).text();
                 var body = toMarkdown($('body').html());
-                $('#ThemableDownload').attr('href','data:text/plain;base64,'+btoa(body));
-                $('#ThemableDownload').attr('download',title+'.md');
-                $('#ThemableDownload')[0].click();
+                $('#ThemeableDownload').attr('href','data:text/plain;base64,'+btoa(body));
+                $('#ThemeableDownload').attr('download',title+'.md');
+                $('#ThemeableDownload')[0].click();
                 return false;
             }
         }
